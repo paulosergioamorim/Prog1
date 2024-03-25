b7716d8781f82079ffb9757796b2e33954cd7dcb
Paulo Sérgio <pamorimmonico@gmail.com>
2024-03-25 10:01:52 -0300

    LDEP1 1-3

--- run_tests.sh
+++ run_tests.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-SOURCE=/mnt/c/Users/pamor/Downloads/$1/input
+SOURCE=/mnt/c/Users/pamor/Downloads/"$1"/input
 
-gcc -o prog $1.c
+gcc -o prog "$1".c
 
-NUM_TESTS=$(find ${SOURCE} -type f | wc -l)
+NUM_TESTS=$(find "$SOURCE" -type f | wc -l)
 
-for n in $(seq 1 $NUM_TESTS);
+for n in $(seq 1 "$NUM_TESTS");
 do
-    ./prog < /mnt/c/Users/pamor/Downloads/$1/input/test_$n > ./test_$n
-    diff /mnt/c/Users/pamor/Downloads/$1/output/test_$n ./test_$n
-    rm ./test_$n
+    ./prog < /mnt/c/Users/pamor/Downloads/"$1"/input/test_"$n" > ./test_"$n"
+    diff /mnt/c/Users/pamor/Downloads/"$1"/output/test_"$n" ./test_"$n"
+    rm ./test_"$n"
 done
 
 rm ./prog
